ce41d7b6f4662f403c6a6b413db18cd79ac29003
Welliton Klein <welliton.fokushima@gmail.com>
2017-11-06 22:48:36 -0200

    Refatorando a aplicação

---
+++ Product.php
@@ -0,0 +1,20 @@
+<?php
+
+class Product
+{
+    private $db;
+
+    public function __construct(IConn $db)
+    {
+        $this->db = $db->connect();
+    }
+
+    public function list(){
+        $query = "select * from products";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+}
